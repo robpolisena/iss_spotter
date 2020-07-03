323bdb45eaf456b8a1dcc48bf5cbf9e52f29b84e
Rob <robpolisena@gmail.com>
2020-07-03 00:15:32 +0000

    runs our main fetch functions

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // index.js
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
-
-
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
+
+  fetchCoordsByIP(ip, (error, coord) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+    console.log(coord);
+  });
 });
+
+
