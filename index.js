0001c71f67f51c363154664dbeb70ce430aff077
Rob <robpolisena@gmail.com>
2020-07-05 23:28:10 +0000

    added printpass times function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
@@ -7,3 +16,5 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   console.log(passTimes);
 });
+
+module.exports = { printPassTimes };
